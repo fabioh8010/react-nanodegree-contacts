671c041ee2077d726bdaac89031fcc8a0724df07
Fábio Henriques <fabio.lacerda@outlook.com>
2018-02-17 03:00:02 -0200

    Add styles, icons, and API file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react'
 
-class ContactList extends Component {
-  render () {
-    const people = this.props.contacts
-
-    return (
-      <ol>
-        {people.map(person => (
-          <li key={person.name}>
-            {person.name}
-          </li>
-        ))}
-      </ol>
-    )
-  }
-}
-
 class App extends Component {
   render () {
     return (
       <div>
-        <ContactList contacts={[
-          { 'name': 'Michael' },
-          { 'name': 'Ryan' },
-          { 'name': 'Tyler' }
-        ]} />
-        <ContactList contacts={[
-          { 'name': 'Fabio' },
-          { 'name': 'Truck' },
-          { 'name': 'Rat' }
-        ]} />
-        <ContactList contacts={[]} />
+        Hello World
       </div>
     )
   }
